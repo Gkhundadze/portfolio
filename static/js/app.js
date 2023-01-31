47a49c81b5ec39c0c3113777c64c7416a7233310
Giga <giorgi.khundadze15b@geolab.edu.ge>
2023-02-01 02:51:25 +0400

    file system update
    corrected scss files
    corrected js modules, created test funcion and some test variables.
    imported json to app.js

---
+++ static/js/app.js
@@ -0,0 +1,8 @@
+import { test } from "./functions.js";
+import { firstVar } from "./variables.js";
+import Developer from './userObj.json' assert {type: 'json'};
+
+
+
+test(firstVar)
+console.log(Developer);
