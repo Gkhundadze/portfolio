18ac7a11b1537c1bfdd53657d2fdad4d4c0e5194
gio <giorgimakasarashvili@gmail.com>
2023-02-06 19:47:10 +0400

    added section2(about)

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,8 @@
-import { test } from "./functions.js";
-import { firstVar } from "./variables.js";
+import * as f from "./functions.js";
+import * as variables from "./variables.js";
 import Developer from './userObj.json' assert {type: 'json'};
 
 
-
-test(firstVar)
-console.log(Developer);
+f.showDevelopmentStack(variables.devStack, Developer);
+f.showProfilePhoto(variables.profilePhoto, Developer);
+f.showAboutMe(variables.aboutMe, Developer);
