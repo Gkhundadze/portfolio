cf42a5131c357f8ec35f8ee3e2087c784ea7942b
Gkhundadze <giorgi.khundadze15b@geolab.edu.ge>
2023-11-24 20:29:47 +0400

    update userObj.json

--- static/js/animation.js
+++ static/js/animation.js
@@ -1,6 +1,7 @@
 // mennu
 const openNavbar = document.querySelector('.openNavbar');
 const closeNavbar = document.querySelector('.closeNavbar');
+
 openNavbar.addEventListener('click', function(){
     header.classList.add('active')
 })
