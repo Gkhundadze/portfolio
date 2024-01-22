78b7d14710e60a12fbcb6899db502f2eb4c6c52d
Gkhundadze <giorgi.khundadze15b@geolab.edu.ge>
2024-01-23 00:53:00 +0400

    add background animation

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,6 @@
+const body = document.body;
+const html = document.documentElement;
+
 getData()
 .then((Developer) => {
 	const workProjects = Developer.workProjects;
@@ -15,9 +18,24 @@ getData()
 	createContainer(0, completedProjects, work_content)
 	scrollBtn(completedProjects);
 	checkNavLinks(navLinksArr)
+	const height = Math.max(
+		body.scrollHeight, 
+		body.offsetHeight,
+		html.clientHeight, 
+		html.scrollHeight, 
+		html.offsetHeight
+		);
+	const width = Math.max(
+		html.clientWidth,
+		html.scrollWidth, 
+		html.offsetWidth, 
+		body.scrollWidth, 
+		body.offsetWidth
+		)
+		const percentage = 3
+		const boxSize = Math.round(width / 100 * percentage)
 	window.onscroll = () => {
 	  let current = "";
-	
 	  sections.forEach((section) => {
 		const sectionTop = section.offsetTop;
 		if (scrollY >= sectionTop - 60) {
@@ -41,14 +59,43 @@ getData()
 			e.target.setAttribute("style", "height:25px !important");
 		}
 	});
+
 	anime({
 		targets: '.dev-name',
 		direction: 'alternate',
 		rotate: '1turn',
 		duration: 1000,
-		easing: 'spring(1, 80, 10, 0)',
+		easing: 'easeOutBack',
 		fontSize: '2rem'
 	  });
+	  generateBackground()
+	  function generateBackground() {
+		const wrapper = document.querySelector('body')
+        for (let i = 1; i < 80; i++) {
+            const element = document.createElement('div')
+            element.classList.add('box')
+			element.style.width = `${boxSize}px`
+			element.style.height = `${boxSize}px`
+            wrapper.prepend(element)
+        }
+		const timeline = anime.timeline({
+			targets: '.box',
+			easing: 'easeInOutQuad',
+			loop: false,
+			delay: 500,
+			duration: 10000
+		})
+		timeline
+			.add({
+				translateY: () => anime.random(0, height),
+				translateX: () => anime.random(0, width + 200),
+				scale: () => anime.random(.5, 6),
+				opacity: () => anime.random(1, 0.3),
+				easing: 'easeInOutQuad',
+			})
+
+		
+	  }
 });
 
 
@@ -62,3 +109,15 @@ async function getData() {
 	  .then((response) => response.json())
 	  .then((data) => data);
 }
+
+// function blurIt(html) {
+// 	html.style.filter = 'blur(1px)'
+// }
+// function unBlurIt(html) {
+// 	html.style.filter = 'unset'
+// }
+// function toggleBlur(html) {
+// 	setInterval(() => {
+// 		html.classList.toggle('blurred')
+// 	}, 300)
+// }
